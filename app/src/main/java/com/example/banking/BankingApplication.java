c6a5dcb03ccef9292517caa5300feee71e1c173a
Trần Chức Thiện <102866314+TranChucThien@users.noreply.github.com>
2026-03-31 10:33:08 +0700

    Update BankingApplication.java

--- app/src/main/java/com/example/banking/BankingApplication.java
+++ app/src/main/java/com/example/banking/BankingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingApplication {
     public static void main(String[] args) {
-        System.out.println("=== Banking App v3 - Layered Docker Build ===");
+        System.out.println("=== Banking App v3 ===");
         SpringApplication.run(BankingApplication.class, args);
     }
 }
